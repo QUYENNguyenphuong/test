bdb32855015b9ef6f6120235107334b8ebc23d96
QUYENNguyenphuong <quyen.nguyen@ceresolutions.com>
2018-10-02 09:45:05 +0700

    add delete button and function delete_item

---
+++ test/js/form.js
@@ -0,0 +1,41 @@
+//
+// // CREATE ELEMENT WHEN CLICK ON ADD BUTTON
+// function newElement() {
+//      var li = document.createElement("li");
+//      var inputvalue = document.getElementById("myinput").value;
+//      var name = document.createTextNode(inputvalue);
+//
+//      li.appendChild(name);
+//      if (inputvalue === '')
+//      {
+//          alert("You must write name !")
+//      }
+//      else{
+//          document.getElementById("ullist").appendChild(li);
+//      }
+//       document.getElementById("myinput").value = "";
+//
+// }
+
+// CREATE ELEMENT WHEN CLICK ON ADD BUTTON
+$(document).ready(function () {
+    $('#btnAdd').click(function () {
+        var name =  $('#myinput').val();
+        if ($('#myinput').val() == '') {
+            alert("You must write name !")
+        }
+        else  {
+            $('#ullist').append('<li>' + name + '<input type="button" class="close" value="Delete" >'+'</li>');
+        }
+        $('#myinput').val("");
+    })
+})
+//function delete
+function delete_item(e, item){
+    e.preventDefault();
+    $(item).parent().remove();
+}
+$(document).on('click', '.close', function(e){
+    var item = this;
+    delete_item(e, item)
+})
