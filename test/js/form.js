c0d90a220edc27cc20634851df7826538c405e4e
QUYENNguyenphuong <quyen.nguyen@ceresolutions.com>
2018-10-04 16:50:43 +0700

    add completed list

--- test/js/form.js
+++ test/js/form.js
@@ -18,44 +18,50 @@
 // }
 // CREATE ELEMENT WHEN CLICK ON ADD BUTTON
 $(document).ready(function () {
-    $('#btnAdd').click(function () {
-        var name =  $('#myinput').val();
-        if ( name == '') {
+    $("#btnAdd").click(function () {
+        var name = $("#myinput").val();
+        if (name == "") {
             alert("You must write name !");
         }
-        else  {
-            $('#ullist').append('<li class="li_item">'+  name + '<input type="button" class="close_li" id="close_li" value="Delete" data-toggle="modal" data-target="#modal">'+'</li>');
+        else {
+            $("#ullist").append('<li class="li_item">' + name + '<input type="checkbox" class="complete_li" id="complete_li" >' + '<input type="button" class="btn btn-primary close_li" id="close_li" value="Delete" data-toggle="modal" data-target="#modal">' + '</li>');
         }
-        $('#myinput').val("");
+        $("#myinput").val("");
     });
-});
+
 //function enter
-$('#myinput').keyup(function (e) {
-    if ( e.keyCode == 13){
-        $('#btnAdd').click();
-    }
-});
-// Delete function
-$(document).on("click",".li_item", function () {
-   var $item = this;
-    $($item).addClass("delete");
-    $("#btn-yes").click(function () {
-        if($($item).hasClass("delete"))
-        {
-            $($item).remove();
-            $('#modal').modal('hide');
+    $("#myinput").keyup(function (e) {
+        if (e.keyCode == 13) {
+            $("#btnAdd").click();
         }
     });
-    $("#btn-no").click(function () {
-        $($item).removeClass("delete");
-        $('#modal').modal('hide');
+// Delete function
+    $(document).on("click", "#close_li", function () {
+        var $item = this;
+        $($item).addClass("delete");
+        $("#btn-yes").click(function () {
+            if ($($item).hasClass("delete")) {
+                $($item).parent().remove();
+                $('#modal').modal("hide");
+            }
+        });
+        $("#btn-no").click(function () {
+            $($item).removeClass("delete");
+            $("#modal").modal("hide");
+        });
+        $(".close").click(function () {
+            $($item).removeClass("delete");
+            $("#modal").modal("hide");
+        })
     });
-    $(".close").click(function () {
-        $($item).removeClass("delete");
-        $('#modal').modal('hide');
+    $(document).on("click","#complete_li", function () {
+       var $item = this;
+       if ($($item).is(":checked")){
+          $("#completed_list").append($($item).parent());
+          $($item).remove();
+       }
     })
 });
-
 
 
 
